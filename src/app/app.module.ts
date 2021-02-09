83be264606728ce80baac028cf481f6cf2c3bac6
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-02-10 00:45:23 +0200

    route2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+import {FullUserComponent} from './components/full-user/full-user.component';
+import {RouterModule} from '@angular/router';
+import {UserResolveService} from './servises/resolve/user-resolve.service';
+
+const routes = [
+  {
+    path: 'users', component: UsersComponent, resolve: {userData: UserResolveService},
+    children: [{path: ':id', component: FullUserComponent}]
+  },
+  {path: '', redirectTo: 'users', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    FullUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/servises/resolve/user-resolve.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {User} from '../../models/User';
+import {UserService} from '../user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserResolveService implements Resolve<User[]> {
+
+  constructor(private userService: UserService) {
+  }
+
+  resolve(): Observable<User[]> | Promise<User[]> {
+        return this.userService.getUsers()
+    }
+
+
+
+
+}
+
