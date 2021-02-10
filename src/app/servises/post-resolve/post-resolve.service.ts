d59cdee73e911b6df2848e5840375a12dee4482b
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-02-10 19:22:26 +0200

    route2

---
+++ src/app/servises/post-resolve/post-resolve.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Resolve} from '@angular/router';
+import {Post} from '../../models/Post';
+import {Observable} from 'rxjs';
+import {PostService} from '../post/post.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostResolveService implements Resolve<Post[]>{
+
+  constructor(private postService: PostService) { }
+
+  resolve(): Observable <Post[]> | Promise<Post[]> | Post[]{
+    return this.postService.getPosts();
+  }
+}
